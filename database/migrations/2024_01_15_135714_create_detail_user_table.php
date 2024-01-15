d067c57657b1e2f1903ada3cef6c15bafc164166
unknown <irmanaila182@gmail.com>
2024-01-15 21:57:31 +0700

    add table detail_user

---
+++ database/migrations/2024_01_15_135714_create_detail_user_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('detail_user', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_user');
+            $table->integer('NIP');
+            $table->string('nama');
+            $table->string('telp')->unique();
+            $table->string('email')->unique();
+            $table->enum('jk', ['Laki-laki', 'Perempuan']);
+            $table->date('tgl_lahir');
+            $table->enum('status', ['PNS', 'Guru Honor Sekolah']);
+            $table->string('PTK');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('detail_user');
+    }
+};
