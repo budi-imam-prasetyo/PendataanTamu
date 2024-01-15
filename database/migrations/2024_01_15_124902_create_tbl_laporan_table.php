b661d6ab52b927d1354a21433fa5ce1437909548
unknown <meisyaauliaar@gmail.com>
2024-01-15 21:19:56 +0700

    tabel laporan

--- database/migrations/2024_01_15_124902_create_tbl_laporan_table.php
+++ database/migrations/2024_01_15_124902_create_tbl_laporan_table.php
@@ -11,14 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tbl_laporan', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('role', ['admin', 'guru','tamu',])->default('tamu');
-            $table->rememberToken();
+            $table->foreign('id_pertemuan')->references('id')->on('categories')->onDelete('cascade');
+            $table->text('hasil_pertemuan');
             $table->timestamps();
         });
     }
@@ -28,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tbl_laporan');
     }
 };
