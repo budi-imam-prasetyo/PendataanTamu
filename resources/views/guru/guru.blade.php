3d2685aa7c3ccccadcd1818c2cd2e0356386008a
upi13 <luthfiyahnur1302@gmail.com>
2024-01-20 20:40:24 +0700

    menambahkan create tabel tamu

---
+++ resources/views/guru/guru.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="w-full">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are Teacher') }}
+                    <a href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();">
+                                            Logout
+                                        </a>
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                            @csrf
+                                        </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TamuController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,7 +22,13 @@ Auth::routes();
 
 Route::middleware('auth')->group(function () {
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::post('/data-tamu', [TamuController::class, 'create'])->name('data-tamu');
 });
+
+Route::middleware(['auth','auth.guru'])->group(function () {
+    Route::get('/guru', [App\Http\Controllers\GuruController::class, 'index'])->name('guru');
+});
+
 Route::middleware(['auth', 'auth.admin'])->group(function () {
     Route::get('/admin', [App\Http\Controllers\AdminController::class, 'index'])->name('admin');
 });
