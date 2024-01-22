c7a5fc92cc00d2abe1b862c3421179eb9e6deb94
unknown <andinirohayani94@gmail.com>
2024-01-22 15:25:43 +0700

    crud pertemuan belum jadi

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+
 
 class HomeController extends Controller
 {
@@ -23,6 +25,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('tamu.home');
+        $user = User::all();
+        return view('tamu.home', compact('user'));
     }
 }
