b2636575b41fe8b66dec9b3ce2b74305ac678f7d
upi13 <luthfiyahnur1302@gmail.com>
2024-01-23 08:31:13 +0700

    Revalidate Back Home dan CRUD Tabel Mapel

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GuruController;
 use App\Http\Controllers\TamuController;
+use App\Http\Controllers\MapelController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,19 +22,25 @@ Route::get('/', function () {
 });
 Auth::routes();
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'revalidate'])->group(function () {
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
     Route::post('/data-tamu', [TamuController::class, 'create'])->name('data-tamu');
 });
 
-Route::middleware(['auth','auth.guru'])->group(function () {
+Route::middleware(['auth','auth.guru', 'revalidate'])->group(function () {
     Route::get('/guru', [App\Http\Controllers\GuruController::class, 'index'])->name('guru');
+    Route::post('/detail-guru', [GuruController::class, 'create'])->name('detailGuru');
 });
 
-Route::middleware(['auth', 'auth.admin'])->group(function () {
+Route::middleware(['auth', 'auth.admin', 'revalidate'])->group(function () {
     Route::get('/admin', [App\Http\Controllers\AdminController::class, 'index'])->name('admin');
     // Route::get('/admin/guru', [App\Http\Controllers\AdminController::class, 'viewGuru'])->name('admin.guru');
     Route::post('/admin/guru', [App\Http\Controllers\AdminController::class, 'addGuru'])->name('admin.addGuru');
     Route::post('/admin/guru/update-guru', [App\Http\Controllers\AdminController::class, 'updateGuru'])->name('admin.updateGuru');
     Route::get('/admin/guru/{email}', [App\Http\Controllers\AdminController::class, 'deleteGuru'])->name('admin.deleteGuru');
+    
+    Route::get('/admin/mapel', [MapelController::class, 'show'])->name('mapel');
+    Route::post('/admin/mapel/add', [MapelController::class, 'create'])->name('admin.mapel');
+    Route::post('/admin/mapel/update', [MapelController::class, 'update'])->name('update.mapel');
+    Route::get('/admin/mapel/delete/{nama}', [MapelController::class, 'destroy'])->name('delete.mapel');
 });
